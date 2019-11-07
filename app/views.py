e8cf8d0845820f6d5d06671a479f98f572c31639
Student <student@donation.local>
2019-11-07 15:40:48 -0600

    completed?

--- app/views.py
+++ app/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from app.models import Coffee, Transaction
+from datetime import datetime
 
 # Create your views here.
+
+
+def home(request):
+    coffees = Coffee.objects.all()
+    return render(request, "app/coffee_list.html", {"coffees": coffees})
+
+
+def transaction_detail(request, id):
+    transaction = Transaction.objects.get(id=id)
+    return render(request, "app/transaction_detail.html", {"transaction": transaction})
+
+
+def buy_coffee(request, id):
+    if request.method == "POST":
+        coffee = Coffee.objects.get(id=id)
+        transaction = coffee.transaction_set.create(
+            time=datetime.now(),
+            item=coffee,
+            pre_tax=coffee.price,
+            tax=round(coffee.price * 0.07, 2),
+        )
+        return redirect("transaction_detail", transaction.id)
+
